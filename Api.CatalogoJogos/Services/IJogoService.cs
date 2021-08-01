ec42f677e3ee9e52e272245e0e2a6813c18d6661
User <User@USER-PC>
2021-07-31 23:37:25 -0300

    Adicionar arquivos de projeto.

---
+++ Api.CatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,18 @@
+﻿using Api.CatalogoJogos.InputModel;
+using Api.CatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.CatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
---
+++ Api.CatalogoJogos/ViewModel/JogoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.CatalogoJogos.ViewModel
+{
+    public class JogoViewModel
+    {
+        public Guid Id { get; set; }
+        
+        public string Nome { get; set; }
+
+        public string Produtora { get; set; }
+
+        public double Preco { get; set; }
+
+
+
+    }
+
+}
